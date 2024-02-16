f30e8345963c37310fd6928d2affddde28a5e16a
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-02-16 16:37:05 +0700

    feat: the "why us" block

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,12 +1,14 @@
 import PopularCategories from "@/app/(marketing)/_components/PopularCategories/PopularCategories";
 import PromoSection from "@/app/(marketing)/_components/PromoSection/PromoSection";
 import LeaderBoard from "@/app/(marketing)/_components/LeaderBoard/LeaderBoard";
+import WhyUs from "@/app/(marketing)/_components/WhyUs/WhyUs";
 export default function Home() {
   return (
     <main className="bg-light-main-bg-main dark:bg-dark-main-bg-main">
       <PromoSection/>
       <PopularCategories/>
       <LeaderBoard/>
+      <WhyUs/>
     </main>
   );
 }
