b6a12beebbbb51f377affed5205a064ea642014c
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-02-18 22:08:29 +0700

    feat: leaderboard carousel

--- src/app/(marketing)/_components/LeaderBoard/LeaderBoard.tsx
+++ src/app/(marketing)/_components/LeaderBoard/LeaderBoard.tsx
@@ -1,7 +1,15 @@
+"use client"
 import HeaderLink from "@/app/(marketing)/_components/HeaderLink/HeaderLink"
 import css from "@/app/(marketing)/_components/LeaderBoard/LeaderBoard.module.scss"
 import LeaderCard from "@/app/(marketing)/_components/LeaderCard/LeaderCard"
-
+import Autoplay from "embla-carousel-autoplay"
+import {
+   Carousel,
+   CarouselContent,
+   CarouselItem,
+   CarouselNext,
+   CarouselPrevious,
+} from "@/shared/ui/carousel"
 interface LeaderBoardItem {
    avatar: string
    fullname: string
@@ -43,6 +51,51 @@ export default function LeaderBoard() {
          rate: 1.0,
          category: ["Копирайтинг", "Парсинг книг"],
       },
+      {
+         avatar: "Perfomer1.png",
+         fullname: "Викторов Диса",
+         href: "/",
+         description:
+            "Копирайт мастер - первое место за месяц, слова, которые завоевали всех!",
+         rate: 1.0,
+         category: ["Копирайтинг", "Парсинг книг"],
+      },
+      {
+         avatar: "Perfomer2.png",
+         fullname: "Иванов Иван",
+         href: "/",
+         description:
+            "Тестовый пользователь - описание пользователя Иванова Ивана.",
+         rate: 3.8,
+         category: ["Тестирование", "Качество"],
+      },
+      {
+         avatar: "Perfomer3.png",
+         fullname: "Петров Петр",
+         href: "/",
+         description:
+            "Тестовый пользователь - описание пользователя Петрова Петра.",
+         rate: 4.2,
+         category: ["Тестирование", "Автоматизация"],
+      },
+      {
+         avatar: "Perfomer1.png",
+         fullname: "Сидоров Сидор",
+         href: "/",
+         description:
+            "Тестовый пользователь - описание пользователя Сидорова Сидора.",
+         rate: 2.7,
+         category: ["Тестирование", "Ручное тестирование"],
+      },
+      {
+         avatar: "Perfomer2.png",
+         fullname: "Александров Саня",
+         href: "/",
+         description:
+            "Тестовый пользователь - описание пользователя Александрова Александра.",
+         rate: 3.5,
+         category: ["Тестирование", "QA"],
+      },
    ]
    return (
       <section className={css.wrapper}>
@@ -53,19 +106,38 @@ export default function LeaderBoard() {
                href='/leaderboard'
             />
          </div>
-         <div className={css.cardsContainer}>
-          {LeaderBoardItems.map((item: LeaderBoardItem, index: number) => (
-              <LeaderCard
-                avatar={item.avatar}
-                fullname={item.fullname}
-                href={item.href}
-                description={item.description}
-                category={item.category}
-                rate={item.rate}
-                key={index}
-              />
-          ))}
-         </div>
+         <Carousel
+            orientation="horizontal"
+            opts={{ align: "center" }}
+            plugins={[
+               Autoplay({
+                  delay: 4000,
+                  stopOnMouseEnter: true,
+               }),
+            ]}
+            className='w-full max-w-[1280px]'
+         >
+            <CarouselContent className={css.cardsContainer}>
+               {LeaderBoardItems.map((item: LeaderBoardItem, index: number) => (
+                  <CarouselItem
+                     key={index}
+                     className='md:basis-1/2 lg:basis-1/3'
+                  >
+                     <LeaderCard
+                        avatar={item.avatar}
+                        fullname={item.fullname}
+                        href={item.href}
+                        description={item.description}
+                        category={item.category}
+                        rate={item.rate}
+                        key={index}
+                     />
+                  </CarouselItem>
+               ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+         </Carousel>
       </section>
    )
 }
